50aeb5c217593fe4256cee864fb9351388295b00
Zarq Khan <77868722+zarq-Khan@users.noreply.github.com>
2024-12-19 00:16:09 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PTCP</title>
+</head>
+<body>
+
+<h1>Hello AbuBaker</h1>
+<p>From PTCP</p>
+
+</body>
+</html>
